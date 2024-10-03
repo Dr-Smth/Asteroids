fd0ef21b669e570c21cced07b12b11250439af2f
Dr-Smth <smith.dr@me.com>
2024-10-03 20:32:37 +0100

    updatable and drawable object containers added to main

--- main.py
+++ main.py
@@ -12,15 +12,24 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
 
-        player.update(dt)
-        player.draw(screen)
+        for object in updatable:
+            object.update(dt)
+        
+        for object in drawable:
+            object.draw(screen)
 
         pygame.display.flip()
 
