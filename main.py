09e7c8761e0df18cdd07eff68441652a587a15a1
Dr-Smth <smith.dr@me.com>
2024-10-03 19:15:37 +0100

    locked to 60fps

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0
 
 def main():
     print("Starting asteroids!")
@@ -13,8 +15,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(0,0,0)
+        screen.fill((0,0,0))
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
